f870451dc76123201037f4f5e1609d0a8e9c7bd7
Ibrahim EL MHADRI <36010272+Gedroke@users.noreply.github.com>
2020-11-15 15:32:03 +0100

    Initial commit

---
+++ src/test/java/com/coronavirus/tracker/coronavirustrackerapp/CoronavirusTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coronavirus.tracker.coronavirustrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
